ed7bf8706d0530dc4adc0d275e2c4fb7b9c05b3c
fargito <fr.farge96@gmail.com>
2022-12-21 10:48:07 +0100

    fix(construct): use event.id for deduplication

--- packages/construct/src/functions/storeEvents/handler.ts
+++ packages/construct/src/functions/storeEvents/handler.ts
@@ -1,7 +1,6 @@
 import { getEnvVariable } from '@swarmion/serverless-helpers';
 import { EventBridgeEvent } from 'aws-lambda';
 import { DocumentClient } from 'aws-sdk/clients/dynamodb';
-import { randomUUID } from 'crypto';
 
 const tableName = getEnvVariable('TEST_TABLE_NAME');
 const documentClient = new DocumentClient();
@@ -13,16 +12,13 @@ export const main = async (
   // see https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/time-to-live-ttl-before-you-start.html
   const timeToLive = Date.now() / 1000 + 15 * 60; // 15 minutes
 
-  // we don't want to overwrite other items so we need to add something to separate them
-  const uuid = randomUUID();
-
   await documentClient
     .put({
       TableName: tableName,
       Item: {
         event,
         PK: event.trailId,
-        SK: `EVENT#${event.time}#${event.source}#${event['detail-type']}#${uuid}`,
+        SK: `EVENT#${event.time}#${event.id}`,
         _ttl: timeToLive,
       },
     })
