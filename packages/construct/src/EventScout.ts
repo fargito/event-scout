7005d5d5ae59f4b58d02dda6c4b3c5b7451b4110
fargito <fr.farge96@gmail.com>
2023-01-03 17:49:40 +0100

    chore(construct): move restApi to separate construct

--- packages/construct/src/EventScout.ts
+++ packages/construct/src/EventScout.ts
@@ -1,5 +1,4 @@
 import { RemovalPolicy } from 'aws-cdk-lib';
-import { RestApi } from 'aws-cdk-lib/aws-apigateway';
 import {
   AttributeType,
   BillingMode,
@@ -10,11 +9,8 @@ import { IEventBus } from 'aws-cdk-lib/aws-events';
 import { BundlingOptions } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
 
-import { ListEventsFunction } from './functions/listEvents/config';
-import { StartEventsTrailFunction } from './functions/startEventsTrail/config';
-import { StopEventsTrailFunction } from './functions/stopEventsTrail/config';
-import { StoreEventsFunction } from './functions/storeEvents/config';
-import { TrailGarbageCollectorFunction } from './functions/trailGarbageCollector/config';
+import { RestApiTrail } from 'constructs/restApiTrail';
+
 import { defaultEsbuildConfig } from './utils/esbuildConfig';
 
 type EventScoutProps = {
@@ -25,6 +21,9 @@ type EventScoutProps = {
 export class EventScout extends Construct {
   endpoint: string;
 
+  /**
+   * The construct to provide all necessary resources for EventScout.
+   */
   constructor(
     scope: Construct,
     id: string,
@@ -45,47 +44,12 @@ export class EventScout extends Construct {
       removalPolicy: RemovalPolicy.DESTROY,
     });
 
-    // add Api
-    const restApi = new RestApi(this, 'RestApi');
-    this.endpoint = restApi.url;
-
-    // Lambda to store events in DynamoDB
-    const { function: storeEvents } = new StoreEventsFunction(
-      this,
-      'StoreEvents',
-      { table, bundling, eventBus },
-    );
-
-    // Lambda to start a trail
-    new StartEventsTrailFunction(this, 'StartEventsTrail', {
+    // create all necessary resource
+    const { endpoint } = new RestApiTrail(this, 'RestApiTrail', {
       table,
-      bundling,
-      restApi,
       eventBus,
-      storeEvents,
-    });
-
-    // Lambda to stop a trail
-    new StopEventsTrailFunction(this, 'StopEventsTrail', {
-      table,
       bundling,
-      restApi,
-      eventBus,
     });
-
-    // Lambda to list the events in the trail
-    new ListEventsFunction(this, 'ListEvents', {
-      table,
-      bundling,
-      restApi,
-    });
-
-    // Lambda to listen to trail items deletion and delete the eventBridge resources
-    // therefore it is safe to not call the stop lambda
-    new TrailGarbageCollectorFunction(this, 'TrailGarbageCollectorFunction', {
-      table,
-      bundling,
-      eventBus,
-    });
+    this.endpoint = endpoint;
   }
 }
---
+++ packages/construct/src/constructs/restApiTrail.ts
@@ -0,0 +1,78 @@
+import { RestApi } from 'aws-cdk-lib/aws-apigateway';
+import { Table } from 'aws-cdk-lib/aws-dynamodb';
+import { IEventBus } from 'aws-cdk-lib/aws-events';
+import { BundlingOptions } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { Construct } from 'constructs';
+
+import { ListEventsFunction } from 'functions/listEvents/config';
+import { StartEventsTrailFunction } from 'functions/startEventsTrail/config';
+import { StopEventsTrailFunction } from 'functions/stopEventsTrail/config';
+import { StoreEventsFunction } from 'functions/storeEvents/config';
+import { TrailGarbageCollectorFunction } from 'functions/trailGarbageCollector/config';
+
+type RestApiTrailProps = {
+  table: Table;
+  bundling: BundlingOptions;
+  eventBus: IEventBus;
+};
+
+export class RestApiTrail extends Construct {
+  endpoint: string;
+
+  /**
+   * all the required resources to implement the rest api trail, i.e. that
+   * can be queried by calling a REST endpoint.
+   *
+   * This is the recommended way for testing
+   */
+  constructor(
+    scope: Construct,
+    id: string,
+    { table, bundling, eventBus }: RestApiTrailProps,
+  ) {
+    super(scope, id);
+
+    // add Api
+    const restApi = new RestApi(this, 'RestApi');
+    this.endpoint = restApi.url;
+
+    // Lambda to store events in DynamoDB
+    const { function: storeEvents } = new StoreEventsFunction(
+      this,
+      'StoreEvents',
+      { table, bundling, eventBus },
+    );
+
+    // Lambda to start a trail
+    new StartEventsTrailFunction(this, 'StartEventsTrail', {
+      table,
+      bundling,
+      restApi,
+      eventBus,
+      storeEvents,
+    });
+
+    // Lambda to stop a trail
+    new StopEventsTrailFunction(this, 'StopEventsTrail', {
+      table,
+      bundling,
+      restApi,
+      eventBus,
+    });
+
+    // Lambda to list the events in the trail
+    new ListEventsFunction(this, 'ListEvents', {
+      table,
+      bundling,
+      restApi,
+    });
+
+    // Lambda to listen to trail items deletion and delete the eventBridge resources
+    // therefore it is safe to not call the stop lambda
+    new TrailGarbageCollectorFunction(this, 'TrailGarbageCollectorFunction', {
+      table,
+      bundling,
+      eventBus,
+    });
+  }
+}
