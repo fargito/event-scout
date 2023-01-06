753ac3ae3c386afb084162507b8dac7078fbeef9
fargito <fr.farge96@gmail.com>
2023-01-06 15:49:44 +0100

    feat(construct): add websocket connection in DynamoDB

--- packages/construct/src/webSocketTrail/functions/onWebSocketDisconnect/handler.ts
+++ packages/construct/src/webSocketTrail/functions/onWebSocketDisconnect/handler.ts
@@ -1,8 +1,20 @@
+import { getEnvVariable } from '@swarmion/serverless-helpers';
 import { APIGatewayProxyWebsocketHandlerV2 } from 'aws-lambda';
+import { DocumentClient } from 'aws-sdk/clients/dynamodb';
+
+const tableName = getEnvVariable('TEST_TABLE_NAME');
+const documentClient = new DocumentClient();
 
 export const main: APIGatewayProxyWebsocketHandlerV2 = async event => {
-  await Promise.resolve();
-  console.log(event);
+  const { connectionId } = event.requestContext;
+
+  // remove the trail item from DynamoDB
+  await documentClient
+    .delete({
+      TableName: tableName,
+      Key: { PK: connectionId, SK: `CONNECTION` },
+    })
+    .promise();
 
   return { statusCode: 200, body: 'Disconnected' };
 };
