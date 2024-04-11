54ad9e68d676981926ef10d404272ff0cabe1e84
fargito <fr.farge96@gmail.com>
2024-04-11 19:00:55 +0200

    test(integration): simplify cf exports retrieval

--- services/validation/vitestIntegrationSetup.ts
+++ services/validation/vitestIntegrationSetup.ts
@@ -1,3 +1,7 @@
+import {
+  CloudFormationClient,
+  ListExportsCommand,
+} from '@aws-sdk/client-cloudformation';
 import { EventBridgeClient } from '@aws-sdk/client-eventbridge';
 import { fromEnv, fromIni } from '@aws-sdk/credential-providers';
 
@@ -12,7 +16,6 @@ import {
   eventBusExportName,
   restApiEndpointExportName,
 } from 'utils/exportNames';
-import { buildGetOutputValue } from 'utils/getOutputValue';
 
 // we need to use an environment variable in order to set the right credentials
 const serverlessEnv = process.env.SLS_ENV ?? defaultEnvironment;
@@ -24,16 +27,27 @@ if (!Object.keys(sharedParams).includes(serverlessEnv)) {
 const profile =
   sharedParams[serverlessEnv as keyof typeof sharedParams].profile;
 
-export const credentials =
+const credentials =
   profile !== '' ? fromIni({ profile: sharedParams.dev.profile }) : fromEnv();
 
-const eventScoutEndpoint = await buildGetOutputValue({ credentials, region })(
-  restApiEndpointExportName,
-);
+const cloudformationClient = new CloudFormationClient({
+  region,
+  credentials,
+});
 
-const eventBusName = await buildGetOutputValue({ credentials, region })(
-  eventBusExportName,
-);
+// For now Cloudformation has no better way than to query all exports and filter client-side
+const cfOutputs = await cloudformationClient.send(new ListExportsCommand({}));
+
+const eventScoutEndpoint = cfOutputs.Exports?.find(
+  o => o.Name === restApiEndpointExportName,
+)?.Value;
+
+const eventBusName = cfOutputs.Exports?.find(
+  o => o.Name === eventBusExportName,
+)?.Value;
+
+if (eventScoutEndpoint === undefined || eventBusName === undefined)
+  throw new Error('unable to retrieve output value');
 
 const eventScoutClient = new EventScoutClient({
   credentials,
